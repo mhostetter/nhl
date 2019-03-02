38c46a38c38c8e1a69810b4f4a1d46b1b984cdd3
mhostetter <matthostetter@gmail.com>
2019-03-02 09:49:29 -0500

    Make has_key/from_key methods explicit and documented

--- nhl/conference.py
+++ nhl/conference.py
@@ -23,3 +23,24 @@ class Conference(Flyweight):
 
     def _key(cls, id, *args, **kwargs):
         return id
+
+    @classmethod
+    def has_key(cls, id):
+        """
+        Check whether flyweight object with specified key has already been created.
+
+        Returns:
+            bool: True if already created, False if not
+        """
+        return super().has_key(id)
+
+    @classmethod
+    def from_key(cls, id):
+        """
+        Return flyweight object with specified key, if it has already been created.
+
+        Returns:
+            nhl.Conference or None: Previously constructed flyweight object with given
+            key or `None` if key not found
+        """
+        return super().from_key(id)
--- nhl/player.py
+++ nhl/player.py
@@ -50,6 +50,27 @@ class Player(Flyweight):
     def _key(cls, id, *args, **kwargs):
         return id
 
+    @classmethod
+    def has_key(cls, id):
+        """
+        Check whether flyweight object with specified key has already been created.
+
+        Returns:
+            bool: True if already created, False if not
+        """
+        return super().has_key(id)
+
+    @classmethod
+    def from_key(cls, id):
+        """
+        Return flyweight object with specified key, if it has already been created.
+
+        Returns:
+            nhl.Player or None: Previously constructed flyweight object with given
+            key or `None` if key not found
+        """
+        return super().from_key(id)
+
     def __post_init__(self, birth_date):
         year = int(birth_date.split("-")[0])
         month = int(birth_date.split("-")[1])
