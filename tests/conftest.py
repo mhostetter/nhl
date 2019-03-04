e7eee1c663f5469b99b19b73b7e640fcfe7b2682
mhostetter <matthostetter@gmail.com>
2019-03-04 00:54:28 -0500

    Add mocks for http requests in unit tests

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import os
+import pytest
+
+@pytest.fixture
+def http_mock(requests_mock):
+    directory = os.path.dirname(__file__)
+    text = open(os.path.join(directory, "data/conference_6.json")).read()
+    requests_mock.get("http://statsapi.web.nhl.com/api/v1/conferences/6", text=text)
+
+    text = open(os.path.join(directory, "data/division_18.json")).read()
+    requests_mock.get("http://statsapi.web.nhl.com/api/v1/divisions/18", text=text)
+
+    text = open(os.path.join(directory, "data/team_15.json")).read()
+    requests_mock.get("http://statsapi.web.nhl.com/api/v1/teams/15", text=text)
+
+    text = open(os.path.join(directory, "data/franchise_24.json")).read()
+    requests_mock.get("http://statsapi.web.nhl.com/api/v1/franchises/24", text=text)
+
+    text = open(os.path.join(directory, "data/player_8471214.json")).read()
+    requests_mock.get("http://statsapi.web.nhl.com/api/v1/people/8471214", text=text)
+
+    text = open(os.path.join(directory, "data/venue_5094.json")).read()
+    requests_mock.get("http://statsapi.web.nhl.com/api/v1/venues/5094", text=text)
